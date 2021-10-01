13c18181721733521512e682b696516bab9b1bf0
shubham vora <shubhamvora05@gmail.com>
2021-10-01 15:02:52 +0530

    comments added

--- Python/Extract Text from PDF/app.py
+++ Python/Extract Text from PDF/app.py
@@ -1,10 +1,11 @@
 # pip install pdfplumber
+# importing pdfplumber
 import pdfplumber
 with pdfplumber.open("Extract Text from PDF\demo.pdf") as pdf:
-    length=len(pdf.pages)
-    print(f"Total number of Page is {length}.")   #find total pages
-    for i in range(1,min(3,length)):    #Print first 2 pages  
+    length = len(pdf.pages)
+    print(f"Total number of Page is {length}.")  # find total pages
+    for i in range(1, min(3, length)):  # Print first 2 pages
         text = pdf.pages[i]
         print()
         print(f"Page {i}:")
-        print(text.extract_text())  #Extract Text from page
+        print(text.extract_text())  # Extract Text from page
