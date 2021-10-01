b6dfcad2e240cf6ddf5b4e6764fedd40132acee5
shubham vora <shubhamvora05@gmail.com>
2021-10-01 08:26:09 +0530

    audio to video file changed

--- Python/Audio From Video/app.py
+++ Python/Audio From Video/app.py
@@ -1,3 +1,4 @@
+# this file can converts video to audio
 import moviepy.editor as mp
 my_clip = mp.VideoFileClip(r"Python/Audio From Video/Video.mp4")
 my_clip.audio.write_audiofile(r"Python/Audio From Video/Output.mp3")
